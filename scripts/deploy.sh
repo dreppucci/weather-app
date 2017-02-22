4ac2b8d2cf1ee22432fb87755138cd6182bb355b
Davide Reppucci <davide.reppucci@gmail.com>
2017-02-22 22:52:10 +0100

    commenting remove all files commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,15 +24,15 @@ npm rebuild node-sass
 npm run build
 
 # Delete and move files.
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv _site/* .
-rm -R _site/
+#find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+#mv _site/* .
+#rm -R _site/
 
 # Push to gh-pages.
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-git add -fA
+#git add -fA
 git commit --allow-empty -m "$(git log -1 --pretty=%B) [ci skip]"
 git push -f $ORIGIN_URL gh-pages
 
