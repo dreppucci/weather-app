53e3e0e4e20f171426db262b01772b9dcef8d142
Davide Reppucci <davide.reppucci@gmail.com>
2017-02-22 15:04:24 +0100

    gh-pages integration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Weather App
 _v 0.1.0_
+
+[![CircleCI](https://circleci.com/gh/dreppucci/weather-app.svg?style=svg)](https://circleci.com/gh/dreppucci/weather-app)
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -86,7 +86,7 @@ module.exports = {
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web'
     },
-    modules: [ paths.nodePaths, 'node_modules'
+    modules: [ paths.nodePaths, 'node_modules' ]
   },
   
   module: {
