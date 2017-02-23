4ac3e499a1a5b76a487ce36d89f7ca07e379f086
Davide Reppucci <davide.reppucci@gmail.com>
2017-02-23 17:02:54 +0100

    npm install command starts only node_modules does not exist. fixed gh-pages folder

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 # ideas used from https://gist.github.com/motemen/8595451
 
-# abort the script if there is a non-zero error
 set -e
-
-# show where we are on the machine
 pwd
 
-remote="https://github.com/dreppucci/weather-app.git"
+remote="$GH_EMAIL"
+siteSource="$SITE_SOURCE"
 
-siteSource="build"
+if [ ! -d "node_modules" ]; then
+  npm install
+else
+  echo "node_modules folder already existing"
+fi
 
-npm install
 npm cache clean
 npm rebuild node-sass
 npm run build
+
+cd ${siteSource}
 
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
-./node_modules/gh-pages/bin/gh-pages -d ${siteSource}
+./../node_modules/gh-pages/bin/gh-pages -d ${siteSource}
