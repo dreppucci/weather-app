1d4f35726987739bf2fc4699bebd940fbbe15f6c
Davide Reppucci <davide.reppucci@gmail.com>
2017-02-23 16:26:12 +0100

    using gh-pages script

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -7,7 +7,7 @@ set -e
 # show where we are on the machine
 pwd
 
-remote=$(git config remote.origin.url)
+remote="https://github.com/dreppucci/weather-app.git"
 
 siteSource="build"
 
@@ -16,28 +16,9 @@ npm cache clean
 npm rebuild node-sass
 npm run build
 
-# make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
-# now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
-git checkout -b gh-pages
-
-# copy over or recompile the new site
-cp -a "../${siteSource}/." .
-
-cd ..
-
-find . -maxdepth 1 ! -name 'gh-pages-branch' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
-mv gh-pages-branch/* .
-rm -rf gh-pages-branch
-
-git add .
-git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
-git push -u --force origin gh-pages:gh-pages #> /dev/null 2>&1
-
-echo "Finished Deployment!"
+./node_modules/gh-pages/bin/gh-pages -d ${siteSource}
